34fd859f257a1f195e029d88de83c9c53fbba7d5
Kevin Zhao <kevinzhao@KevindeAir.ycdsb.ca>
2019-12-20 10:24:45 -0500

    load texture

---
+++ test programs/Insert pictures.py
@@ -0,0 +1,16 @@
+import os
+import arcade
+
+arcade.open_window(600, 600, 'title')
+arcade.set_background_color(arcade.color.WHITE)
+
+file_path = os.path.abspath(os.path.join(os.path.dirname(__file__),os.path.pardir))
+os.chdir(file_path)
+
+arcade.start_render()
+
+texture = arcade.load_texture('Artworks/example.png')
+arcade.draw_texture_rectangle(300, 300, texture.width, texture.height, texture, 0)
+
+arcade.finish_render()
+arcade.run()
